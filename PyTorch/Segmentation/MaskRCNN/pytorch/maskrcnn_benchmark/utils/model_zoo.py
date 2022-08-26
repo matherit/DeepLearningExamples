2c916b9665c49521489d5c2d6d2c29c70286f325
Isaac T. Mathers <matherit@amazon.com>
2022-08-26 16:47:23 +0000

    Fix import.

--- PyTorch/Segmentation/MaskRCNN/pytorch/maskrcnn_benchmark/utils/model_zoo.py
+++ PyTorch/Segmentation/MaskRCNN/pytorch/maskrcnn_benchmark/utils/model_zoo.py
@@ -7,9 +7,14 @@ try:
     from torch.utils.model_zoo import urlparse
     from torch.utils.model_zoo import HASH_REGEX
 except:
-    from torch.hub import _download_url_to_file
-    from torch.hub import urlparse
-    from torch.hub import HASH_REGEX
+    try:
+        from torch.hub import _download_url_to_file
+        from torch.hub import urlparse
+        from torch.hub import HASH_REGEX
+    except:
+        from torch.hub import download_url_to_file as _download_url_to_file
+        from torch.hub import urlparse
+        from torch.hub import HASH_REGEX
 
 from maskrcnn_benchmark.utils.comm import is_local_main_process
 from maskrcnn_benchmark.utils.comm import synchronize
